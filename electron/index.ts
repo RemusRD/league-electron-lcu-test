51a31ee8e12efb1dc50ee9d9580d06d7317756b5
Remus Richard Dumitrache <RemusRichardDumitrache@gmail.com>
2022-08-29 13:23:21 +0200

    WIP

--- electron/index.ts
+++ electron/index.ts
@@ -1,18 +1,19 @@
 // @ts-nocheck
-import {join} from 'path';
+import { join } from 'path';
 import LCUConnector from 'lcu-connector';
 import fetch from 'electron-fetch';
 
 // Packages
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as electron from 'electron';
-import {app, BrowserWindow, ipcMain} from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import isDev from 'electron-is-dev';
-import {ElectronAuthProvider} from '@twurple/auth-electron';
-import {AccessToken} from '@twurple/auth';
-import {ApiClient} from '@twurple/api';
-import {RiotWSProtocol} from './RiotWSProtocol';
-import {clientId} from './config';
+import { ElectronAuthProvider } from '@twurple/auth-electron';
+import { AccessToken } from '@twurple/auth';
+import { ApiClient } from '@twurple/api';
+import { HelixCreatePredictionData } from 'twitch';
+import { RiotWSProtocol } from './RiotWSProtocol';
+import { clientId } from './config';
 
 electron.app.commandLine.appendSwitch('ignore-certificate-errors');
 
@@ -21,187 +22,193 @@ const width = 600;
 let clientConnected = false;
 let riotCredentials: any = null;
 let winApp: any = null;
-let twitchCredentials: AccessToken = null;
+let twitchUser: any = null;
 
 const authProvider = new ElectronAuthProvider({
-    clientId,
-    redirectUri: 'http://localhost:3001/auth/callback'
+  clientId,
+  redirectUri: 'http://localhost:3001/auth/callback'
 });
 
 function createWindow() {
-    // Create the browser window.
-    const window: BrowserWindow = new BrowserWindow({
-        width,
-        height,
-        //  change to false to use AppBar
-        frame: false,
-        show: true,
-        resizable: true,
-        fullscreenable: true,
-        webPreferences: {
-            preload: join(__dirname, 'preload.js')
-        }
-    });
-
-    const port = process.env.PORT || 3000;
-    const url = isDev ? `http://localhost:${port}` : join(__dirname, '../src/out/index.html');
-
-    // and load the index.html of the app.
-    if (isDev) {
-        window?.loadURL(url);
-    } else {
-        window?.loadFile(url);
+  // Create the browser window.
+  const window: BrowserWindow = new BrowserWindow({
+    width,
+    height,
+    //  change to false to use AppBar
+    frame: false,
+    show: true,
+    resizable: true,
+    fullscreenable: true,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js')
     }
-    // Open the DevTools.
-    // window.webContents.openDevTools();
+  });
 
-    // For AppBar
-    ipcMain.on('minimize', () => {
-        // eslint-disable-next-line no-unused-expressions
-        window.isMinimized() ? window.restore() : window.minimize();
-        // or alternatively: win.isVisible() ? win.hide() : win.show()
-    });
-    ipcMain.on('maximize', () => {
-        // eslint-disable-next-line no-unused-expressions
-        window.isMaximized() ? window.restore() : window.maximize();
-    });
+  const port = process.env.PORT || 3000;
+  const url = isDev ? `http://localhost:${port}` : join(__dirname, '../src/out/index.html');
 
-    ipcMain.on('close', () => {
-        window.close();
-    });
-    return window;
+  // and load the index.html of the app.
+  if (isDev) {
+    window?.loadURL(url);
+  } else {
+    window?.loadFile(url);
+  }
+  // Open the DevTools.
+  // window.webContents.openDevTools();
+
+  // For AppBar
+  ipcMain.on('minimize', () => {
+    // eslint-disable-next-line no-unused-expressions
+    window.isMinimized() ? window.restore() : window.minimize();
+    // or alternatively: win.isVisible() ? win.hide() : win.show()
+  });
+  ipcMain.on('maximize', () => {
+    // eslint-disable-next-line no-unused-expressions
+    window.isMaximized() ? window.restore() : window.maximize();
+  });
+
+  ipcMain.on('close', () => {
+    window.close();
+  });
+  return window;
 }
 
 async function getCurrentSummoner() {
-    const base64Creds = Buffer.from(`riot:${riotCredentials.password}`).toString('base64');
-    const response = await fetch(`https://127.0.0.1:${riotCredentials.port}/lol-summoner/v1/current-summoner`, {
-        headers: {Authorization: `Basic ${base64Creds}`}
-    });
-    return response;
+  const base64Creds = Buffer.from(`riot:${riotCredentials.password}`).toString('base64');
+  const response = await fetch(`https://127.0.0.1:${riotCredentials.port}/lol-summoner/v1/current-summoner`, {
+    headers: { Authorization: `Basic ${base64Creds}` }
+  });
+  return response;
 }
 
 async function notifySummonerInfo() {
-    if (!clientConnected) return;
-    try {
-        const response = await getCurrentSummoner();
-        const summonerInfo = await response.json();
-        if (response.status === 200 && summonerInfo.displayName) {
-            winApp.webContents.send('summonerInfo', {
-                id: summonerInfo.accountId,
-                name: summonerInfo.displayName,
-                level: summonerInfo.summonerLevel
-            });
-        } else {
-            setTimeout(async () => {
-                await notifySummonerInfo();
-            }, 1000);
-        }
-    } catch (e) {
-        setTimeout(async () => {
-            await notifySummonerInfo();
-        }, 1000);
+  if (!clientConnected) return;
+  try {
+    const response = await getCurrentSummoner();
+    const summonerInfo = await response.json();
+    if (response.status === 200 && summonerInfo.displayName) {
+      winApp.webContents.send('summonerInfo', {
+        id: summonerInfo.accountId,
+        name: summonerInfo.displayName,
+        level: summonerInfo.summonerLevel
+      });
+    } else {
+      setTimeout(async () => {
+        await notifySummonerInfo();
+      }, 1000);
     }
+  } catch (e) {
+    setTimeout(async () => {
+      await notifySummonerInfo();
+    }, 1000);
+  }
 }
 
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
-    winApp = createWindow();
-    twitchCredentials = await authProvider.getAccessToken(['chat:edit', 'chat:read', 'user:read:email']);
-    const apiClient = new ApiClient(
-        {authProvider}
-    );
-    const twitchUser = await apiClient.users.getMe();
-    console.log("Connected to Twitch as " + twitchUser.displayName);
-    winApp.webContents.send('twitchConnection',
-        {
-            twitchUserId: twitchUser.id,
-            username: twitchUser.displayName,
-        }
-    );
+  winApp = createWindow();
+  const twitchCredentials = await authProvider.getAccessToken([
+    'chat:edit',
+    'chat:read',
+    'user:read:email',
+    'channel:manage:predictions'
+  ]);
+  const apiClient = new ApiClient({ authProvider });
+  twitchUser = await apiClient.users.getMe();
+  winApp.webContents.send('twitchConnection', {
+    twitchUserId: twitchUser.id,
+    username: twitchUser.displayName
+  });
 
-    const connector = new LCUConnector();
-    connector.on('connect', async (data) => {
-        winApp.webContents.send('clientStatus', {connected: true});
-        clientConnected = true;
-        riotCredentials = data;
-        const url = `wss://${riotCredentials.username}:${riotCredentials.password}@127.0.0.1:${riotCredentials.port}`;
-        const ws = new RiotWSProtocol(url);
-        ws.on('open', () => {
-            ws.subscribe('OnJsonApiEvent', (event: any) => {
-                if (event?.data?.map?.gameMode === 'TFT') {
-                    if (event?.uri === '/lol-gameflow/v1/session') {
-                        console.log(event);
-                        if (event?.data?.phase === 'InProgress') {
-                            apiClient.chat.sendAnnouncement(
-                                twitchUser.id,
-                                twitchUser.id,
-                                {message: 'Game started'}
-                            )
-                            console.log("Game started");
-                        }
-                        if (event?.data.phase === 'EndOfGame') {
-                            apiClient.chat.sendAnnouncement(
-                                twitchUser.id,
-                                twitchUser.id,
-                                {
-                                    message: 'Game ended'
-                                }
-                            )
-                            console.log("Game ended");
-                            notifyEndOfGame();
-                        }
-                    }
-                }
-            });
-        });
-        await notifySummonerInfo();
+  const connector = new LCUConnector();
+  connector.on('connect', async (data) => {
+    winApp.webContents.send('clientStatus', { connected: true });
+    clientConnected = true;
+    riotCredentials = data;
+    const url = `wss://${riotCredentials.username}:${riotCredentials.password}@127.0.0.1:${riotCredentials.port}`;
+    const ws = new RiotWSProtocol(url);
+    ws.on('open', () => {
+      ws.subscribe('OnJsonApiEvent', (event: any) => {
+        if (event?.data?.map?.gameMode === 'TFT') {
+          if (event?.uri === '/lol-gameflow/v1/session') {
+            console.log(event);
+            if (event?.data?.phase === 'InProgress') {
+              apiClient.chat.sendAnnouncement(twitchUser.id, twitchUser.id, { message: 'Game started' });
+              apiClient.predictions.createPrediction(
+                twitchUser.id,
+                new (class implements HelixCreatePredictionData {
+                  title = '¿Qué top quedamos?';
+
+                  outcomes = ['1,2', '3,4', '5,6', '7,8'];
+
+                  autoLockAfter = '120';
+                })()
+              );
+              console.log('Game started');
+            }
+            if (event?.data.phase === 'EndOfGame') {
+              apiClient.chat.sendAnnouncement(twitchUser.id, twitchUser.id, {
+                message: 'Game ended'
+              });
+              console.log('Game ended');
+              notifyEndOfGame();
+            }
+          }
+        }
+      });
     });
-    connector.on('disconnect', () => {
-        console.log('disconnected');
-        winApp.webContents.send('clientStatus', {connected: false});
-        clientConnected = false;
-    });
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-    connector.start();
+    await notifySummonerInfo();
+  });
+  connector.on('disconnect', () => {
+    console.log('disconnected');
+    winApp.webContents.send('clientStatus', { connected: false });
+    clientConnected = false;
+  });
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+  connector.start();
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 ipcMain.on('message', (event, message: any) => {
-    if (message === 'clientStatusCheck') {
-        event.sender.send('clientStatus', {connected: clientConnected});
-    }
+  if (message === 'clientStatusCheck') {
+    event.sender.send('clientStatus', { connected: clientConnected });
+    winApp.webContents.send('twitchConnection', {
+      twitchUserId: twitchUser.id,
+      username: twitchUser.displayName
+    });
+  }
 });
 ipcMain.on('message', async (event, message: any) => {
-    if (message === 'summonerInfoCheck') {
-        await notifySummonerInfo();
-    }
+  if (message === 'summonerInfoCheck') {
+    await notifySummonerInfo();
+  }
 });
 
 async function getEndOfGame() {
-    const base64Creds = Buffer.from(`riot:${riotCredentials.password}`).toString('base64');
-    const response = await fetch(`https://127.0.0.1:${riotCredentials.port}/lol-end-of-game/v1/tft-eog-stats`, {
-        headers: {Authorization: `Basic ${base64Creds}`}
-    });
-    return response;
+  const base64Creds = Buffer.from(`riot:${riotCredentials.password}`).toString('base64');
+  const response = await fetch(`https://127.0.0.1:${riotCredentials.port}/lol-end-of-game/v1/tft-eog-stats`, {
+    headers: { Authorization: `Basic ${base64Creds}` }
+  });
+  return response;
 }
 
 async function notifyEndOfGame() {
-    if (!clientConnected) return;
-    try {
-        const response = await getEndOfGame();
-        const endOfGame = await response.json();
-        const {rank} = endOfGame.localPlayer;
-    } catch (e) {
-        setTimeout(async () => {
-            await notifyEndOfGame();
-        }, 1000);
-    }
+  if (!clientConnected) return;
+  try {
+    const response = await getEndOfGame();
+    const endOfGame = await response.json();
+    const { rank } = endOfGame.localPlayer;
+  } catch (e) {
+    setTimeout(async () => {
+      await notifyEndOfGame();
+    }, 1000);
+  }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
 import React, {useEffect, useState} from 'react';
+import AppBar from "./AppBar";
 
 function App() {
     const [riotClientInfo, setRiotClientInfo] = useState(false);
-    const [twitchConnection, setTwitchConnection] = useState<{[key: string]: any}>({});
+    const [twitchConnection, setTwitchConnection] = useState<{ [key: string]: any }>({});
 
     useEffect(() => {
         if (window.Main) {
             window.Main.on('clientStatus', (message) => {
                 setRiotClientInfo(message.connected);
             });
+        }
+    }, [riotClientInfo]);
+
+    useEffect(() => {
+        if (window.Main) {
             window.Main.on('twitchConnection', (message) => {
                 setTwitchConnection(message);
             });
         }
+    }, [twitchConnection]);
+    useEffect(() => {
         window.Main.sendMessage('clientStatusCheck');
-    }, [riotClientInfo]);
+    }, []);
 
     return (
-        <div className="flex flex-row">
-            <div className="flex flex-col w-1/2">
-                <div>
-                    <p className="text-2xl">League client Status: {riotClientInfo ? 'Connected' : 'Disconnected'}</p>
-                </div>
-                <div>
-                    <p className="text-2xl">Twitch connection : {twitchConnection?.username}</p>
+        <div className="flex flex-col h-screen">
+            <div className="flex-none">
+                <AppBar/>
+            </div>
+            <div className=" flex flex-col justify-center items-center h-screen bg-gray-800 space-y-4">
+                <h1 className="text-2xl text-gray-200">Vite + React + Typescript + Electron + Tailwind</h1>
+                <div className="flex flex-col space-y-4 items-center">
+                    <div className="flex space-x-3">
+                        <h1 className="text-xl text-gray-50">💝 Welcome 💝, now send a massage to the Main 📩📩</h1>
+                    </div>
+                    <div>
+                        <h4 className=" text-yellow-200"></h4>
+                    </div>
                 </div>
             </div>
-        </div>);
-
+            <div/>
+        </div>
+    );
 }
 
 export default App;
