6c80bc401bfe5529b5e82b56709b624a4dccadc3
victor.cosqui <victor.cosqui@personio.de>
2022-08-30 17:46:58 +0200

    send message upon game start

--- electron/TwitchAdapter.ts
+++ electron/TwitchAdapter.ts
@@ -3,27 +3,32 @@
 import {clientId} from './config';
 import {ElectronAuthProvider} from "@twurple/auth-electron";
 import {ApiClient} from "twitch";
+import {ChatClient} from "twitch-chat-client";
 
 export default class TwitchAdapter {
 
-    authProvider = new ElectronAuthProvider({
-        clientId,
-        redirectUri: 'http://localhost:3000/auth/callback'
-    });
+  authProvider = new ElectronAuthProvider({
+    clientId,
+    redirectUri: 'http://localhost:3000/auth/callback'
+  });
 
-    async connect() : TwitchUser {
-        const twitchCredentials = await this.authProvider.getAccessToken([
-           'chat:edit',
-           'chat:read',
-           'user:read:email',
-           'channel:manage:predictions'
-         ]);
-        const apiClient = new ApiClient({ authProvider : this.authProvider });
-        const twitchUser = await apiClient.helix.users.getMe()
-        return new TwitchUser(twitchUser.displayName);
-    }
+  async connect(): TwitchUser {
+    const twitchCredentials = await this.authProvider.getAccessToken([
+      'chat:edit',
+      'chat:read',
+      'user:read:email',
+      'channel:manage:predictions'
+    ]);
+    const apiClient = new ApiClient({authProvider: this.authProvider});
+    const twitchUser = await apiClient.helix.users.getMe()
+    const chatClient = new ChatClient(this.authProvider, {channels: [twitchUser.displayName]})
+    await chatClient.say('#' + twitchUser.displayName, 'hello from app')
+    await chatClient.send('hello from app with send')
+    return new TwitchUser(twitchUser.displayName);
+  }
 }
 
 class TwitchUser {
-    constructor(readonly username: String) {}
+  constructor(readonly username: String) {
+  }
 }
--- electron/index.ts
+++ electron/index.ts
@@ -179,7 +179,7 @@ ipcMain.on('message', async (event, message: any) => {
 ipcMain.on('message', async (event, message: any) => {
     if (message === 'enable-predictions') {
         console.log("predictions enabled");
-        tftAdapter.onGameStarted(createPrediction);
+        tftAdapter.onGameStarted(() => console.log('prediction callback invoked'));
     }
 });
 
