5ac1e158001f34287b43d1e5c9e16d537af44b9a
Remus Richard Dumitrache <RemusRichardDumitrache@gmail.com>
2022-08-30 18:06:31 +0200

    Twitch chat message layout

--- electron/TwitchAdapter.ts
+++ electron/TwitchAdapter.ts
@@ -7,28 +7,37 @@ import {ChatClient} from "twitch-chat-client";
 
 export default class TwitchAdapter {
 
-  authProvider = new ElectronAuthProvider({
-    clientId,
-    redirectUri: 'http://localhost:3000/auth/callback'
-  });
+    authProvider = new ElectronAuthProvider({
+        clientId,
+        redirectUri: 'http://localhost:3000/auth/callback'
+    });
 
-  async connect(): TwitchUser {
-    const twitchCredentials = await this.authProvider.getAccessToken([
-      'chat:edit',
-      'chat:read',
-      'user:read:email',
-      'channel:manage:predictions'
-    ]);
-    const apiClient = new ApiClient({authProvider: this.authProvider});
-    const twitchUser = await apiClient.helix.users.getMe()
-    const chatClient = new ChatClient(this.authProvider, {channels: [twitchUser.displayName]})
-    await chatClient.say('#' + twitchUser.displayName, 'hello from app')
-    await chatClient.send('hello from app with send')
-    return new TwitchUser(twitchUser.displayName);
-  }
+    chatClient: ChatClient;
+
+    twitchUser: TwitchUser;
+
+    async connect(): TwitchUser {
+        const twitchCredentials = await this.authProvider.getAccessToken([
+            'chat:edit',
+            'chat:read',
+            'user:read:email',
+            'channel:manage:predictions'
+        ]);
+        const apiClient = new ApiClient({authProvider: this.authProvider});
+        const twitchUser = await apiClient.helix.users.getMe()
+        this.chatClient = new ChatClient(this.authProvider, {channels: [twitchUser.displayName]})
+        await this.chatClient.connect();
+        this.twitchUser = new TwitchUser(twitchUser.displayName)
+        return this.twitchUser;
+    }
+
+    async helloChat(message: string) {
+        await this.chatClient.say(this.twitchUser.username, message);
+    }
 }
 
+
 class TwitchUser {
-  constructor(readonly username: String) {
-  }
+    constructor(readonly username: String) {
+    }
 }
